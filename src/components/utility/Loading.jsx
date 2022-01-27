5a2fe25bd160fca954dca79c77f6c6eb2dea35f5
Deepak Bisht <32607176+Deepak5597@users.noreply.github.com>
2022-01-27 13:37:58 +0530

    Delete src/components directory

--- src/components/utility/Loading.jsx
+++
@@ -1,9 +0,0 @@
-function Loading() {
-    return (
-        <div className="flex justify-center items-center text-xl font-bold">
-            Loading....
-        </div>
-    );
-}
-
-export default Loading;
