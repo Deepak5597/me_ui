fe392df0475325cdbdac7c5eef3aea9113920129
Deepak Bisht <32607176+Deepak5597@users.noreply.github.com>
2022-01-27 17:44:13 +0530

    Changes for responsive Navigation
    Made Below Changes
    ---
    Responsive Navigation Bar
    Changes the favicon icon to brand logo

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,15 +3,17 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import { Suspense } from 'react';
 import Loading from '../utility/Loading';
+import useGlobal from '../../hooks/useGlobal';
 function Layout() {
+    const { sidebarState } = useGlobal();
     return (
         <>
             <Header />
-            <div className="h-[calc(100vh-4rem)] overflow-y-scroll flex">
-                <div className="hidden md:flex w-1/5">
+            <div className="h-[calc(100vh-4rem)] overflow-y-scroll flex relative">
+                <div className={`${sidebarState ? 'absolute w-full top-0 left-0 height-screen' : 'hidden'} lg:flex lg:w-1/5`}>
                     <Sidebar />
                 </div>
-                <div className="w-full md:w-4/5">
+                <div className="w-full lg:w-4/5">
                     <Suspense fallback={<Loading />}>
                         <Outlet />
                     </Suspense>
